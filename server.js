eb035aeb8539a9e3bad6f2c7a725578bc96fca92
Yaroslav Dmitriev <113548499+ElonMusk2002@users.noreply.github.com>
2023-05-02 08:19:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
+
  app.use(express.static(path.join(__dirname, "public")));
- app.listen(3000, () => console.log("Server running on port 3000"));
+
+
+app.listen(3000, () => console.log("Server running on port 3000"));
